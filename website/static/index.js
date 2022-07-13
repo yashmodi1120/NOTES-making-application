2833f593505713835b91e4f9b80f78c2996166de
yashmodi1120 <79049807+yashmodi1120@users.noreply.github.com>
2022-07-13 14:28:58 +0530

    Add files via upload

---
+++ website/static/index.js
@@ -0,0 +1,10 @@
+function deleteNode(noteId)
+{
+    fetch('delete-note' ,
+    {
+        method : 'POST',
+        body : JSON.stringify({noteId : noteId})
+    }).then((_res) => {
+        window.location.href = '/';
+    })
+}
